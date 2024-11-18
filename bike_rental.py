41b3076f6188a859e720788e7c2eea0de893cfcb
Łukasz Tugeman <tugeman.lukasz@gmail.com>
2024-11-18 11:14:06 +0100

    Add calculate_cost function

--- bike_rental.py
+++ bike_rental.py
@@ -3,23 +3,28 @@ import datetime
 import smtplib
 import os
 
-def rent_bike(customer_name, rental_duration):
-    None
+# def rent_bike(customer_name:str, rental_duration:int):
 
 def calculate_cost(rental_duration):
-    None
+    return f"{10+(rental_duration-1)*5}zł"
 
-def save_rental(rental):
-    None
+# def save_rental(rental):
+#     json.dumps(x)
 
-def load_rentals():
-    None
+# def load_rentals():
+#     None
 
-def cancel_rental(customer_name):
-    None
+# def cancel_rental(customer_name):
+#     None
 
-def send_rental_invoice_email(customer_email, rental_details):
-    None
+# def send_rental_invoice_email(customer_email, rental_details):
+#     None
 
-def generate_daily_report():
-    None
+# def generate_daily_report():
+#     None
+
+# def main():
+#     n = str(input("Podaj co chcesz zrobić"))
+#     match n != "exit":
+#         case "rent":
+#             customer_name = input("Podaj imię klienta: ")
