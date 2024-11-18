4951c8ee7a968940262af163619402e999bdf748
Łukasz Tugeman <tugeman.lukasz@gmail.com>
2024-11-18 10:39:12 +0100

    Add imports

---
+++ bike_rental.py
@@ -0,0 +1,25 @@
+import json
+import datetime
+import smtplib
+import os
+
+def rent_bike(customer_name, rental_duration):
+    None
+
+def calculate_cost(rental_duration):
+    None
+
+def save_rental(rental):
+    None
+
+def load_rentals():
+    None
+
+def cancel_rental(customer_name):
+    None
+
+def send_rental_invoice_email(customer_email, rental_details):
+    None
+
+def generate_daily_report():
+    None
