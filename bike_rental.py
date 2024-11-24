779f7bb864ef80f2c0723ba6499ea6ca5a46bfe8
Łukasz Tugeman <tugeman.lukasz@gmail.com>
2024-11-24 15:53:51 +0100

    Implement rent_bike function

--- bike_rental.py
+++ bike_rental.py
@@ -3,13 +3,20 @@ import datetime
 import smtplib
 import os
 
-# def rent_bike(customer_name:str, rental_duration:int):
+def rent_bike(customer_name:str, rental_duration:int):
+    price = calculate_cost(rental_duration)
+    rental = {
+            "Imię" : customer_name,
+            "długość wynajmu" : rental_duration,
+            "cena" : price
+        }
+    save_rental(rental)
 
 def calculate_cost(rental_duration:int) ->str:
     return f"{10+(rental_duration-1)*5}zł"
 
-# def save_rental(rental):
-#     json.dumps(x)
+def save_rental(rental:dict):
+    None
 
 # def load_rentals():
 #     None
